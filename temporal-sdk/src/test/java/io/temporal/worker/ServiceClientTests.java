292bf4532f783a7192e370623a620268ea8a97a7
Mincong HUANG <mincong.h@gmail.com>
2021-10-09 19:00:00 +0200

    testNewUntypedWorkflowStubWithAssertions

--- temporal-sdk/src/test/java/io/temporal/worker/ServiceClientTests.java
+++ temporal-sdk/src/test/java/io/temporal/worker/ServiceClientTests.java
@@ -86,6 +86,40 @@ public class ServiceClientTests {
   }
 
   @Test
+  public void testNewUntypedWorkflowStubWithAssertions() {
+    // Arrange
+    String taskQueueName = "veryLongWorkflow";
+
+    // setup server
+    WorkerFactory factory = testEnv.getWorkerFactory();
+    Worker worker = factory.newWorker(taskQueueName);
+    worker.addWorkflowImplementationFactory(
+        ActivitiesWorkflow.class,
+        () -> {
+          return new ActivitiesWorkflow() {
+            @Override
+            public String execute(WorkflowParams params) {
+              assertEquals("JUnit", params.sender); // HERE
+              return "assertions passed";
+            }
+          };
+        });
+    factory.start();
+
+    // setup client
+    WorkflowOptions workflowOptions =
+        WorkflowOptions.newBuilder().setTaskQueue(taskQueueName).build();
+    WorkflowStub workflow =
+        testEnv.getWorkflowClient().newUntypedWorkflowStub("ActivitiesWorkflow", workflowOptions);
+
+    WorkflowParams params = new WorkflowParams();
+    params.sender = "JUnit";
+    workflow.start(params);
+
+    assertEquals("assertions passed", workflow.getResult(String.class));
+  }
+
+  @Test
   public void testBlockingStub() {
     // Arrange
     String taskQueueName = "veryLongWorkflow";
