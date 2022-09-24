3a2668753854a9ee86d9d7a82cfd4b79f2b3a463
Mincong Huang <mincong.h@gmail.com>
2022-09-24 21:33:17 +0200

    stubs

--- temporal-serviceclient/src/main/java/io/temporal/serviceclient/WorkflowServiceStubs.java
+++ temporal-serviceclient/src/main/java/io/temporal/serviceclient/WorkflowServiceStubs.java
@@ -28,7 +28,21 @@ import io.temporal.internal.testservice.InProcessGRPCServer;
 import java.time.Duration;
 import javax.annotation.Nullable;
 
-/** Initializes and holds gRPC blocking and future stubs. */
+/**
+ * Initializes and holds gRPC blocking and future stubs.
+ *
+ * In gRPC, a client application can directly call a method on a server application on a different
+ * machine as if it were a local object, making it easier for you to create distributed
+ * applications and services. As in many RPC systems, gRPC is based around the idea of defining a
+ * service, specifying the methods that can be called remotely with their parameters and return
+ * types.
+ *   - On the server side, the server implements this interface and runs a gRPC server to handle
+ *     client calls.
+ *   - On the client side, the client has a stub (referred to as just a client in some languages)
+ *     that provides the same methods as the server.
+ *
+ * See https://www.grpc.io/docs/what-is-grpc/introduction/#overview
+ */
 public interface WorkflowServiceStubs
     extends ServiceStubs<
         WorkflowServiceGrpc.WorkflowServiceBlockingStub,
